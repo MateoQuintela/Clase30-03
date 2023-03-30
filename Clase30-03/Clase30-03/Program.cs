1f0906b4018d5c1523bf7181f19080fdfd516e05
Mateo <m4teo2001@gmail.com>
2023-03-30 20:49:32 -0300

    Segundo commit

---
+++ Clase30-03/Clase30-03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase30_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola gato pardo");
+        }
+
+        public class Clase1
+        {
+
+            public void Hola() { Console.WriteLine("Hola k haces gato "); }
+
+
+
+        }
+    }
+}
